1633501d184ebc5ca8e6a4ec322f46e71bf28ded
Braedon Vickers <braedon@movio.co>
2016-03-14 13:07:31 +1300

    Merge label dicts in a way that is supported pre python 3.5

--- exporter/parser.py
+++ exporter/parser.py
@@ -3,7 +3,9 @@ def parse_response(value_columns, response, metric=[], labels={}):
 
     for row in response:
       result_labels = {column: (str(row[column]),) for column in row if column not in value_columns}
+      final_labels = labels.copy()
+      final_labels.update(result_labels)
       for value_column in value_columns:
-        result.append((metric + [value_column], {**labels, **result_labels}, row[value_column]))
+        result.append((metric + [value_column], final_labels, row[value_column]))
 
     return result
